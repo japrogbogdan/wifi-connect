8821c7a15fed73ec8aae0c21356ff61769b960ea
obolsh <obolsh@gmail.com>
2021-07-09 13:39:18 +0300

    Update demo app

--- demo/src/main/java/io/connect/wifi/demo/MainActivity.kt
+++ demo/src/main/java/io/connect/wifi/demo/MainActivity.kt
@@ -2,38 +2,93 @@ package io.connect.wifi.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.connect.wifi.demo.databinding.ActivityMainBinding
 import io.connect.wifi.sdk.WiFiSessionStatus
 import io.connect.wifi.sdk.WifiSession
 import io.connect.wifi.sdk.WifiSessionCallback
+import ui.helper.LocalCache
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var wifi: WifiSession
+    private lateinit var binding: ActivityMainBinding
+    private val cache: LocalCache by lazy { LocalCache(this) }
+
+    private val API_KEY =
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJwaWQiOiIxIiwic3ViIjoiZGM4ZDI4NGUtOWU3Mi00NmExLTk5YTctNDRhZmM2NjAzZjk5IiwiaWF0IjoxNjIxODc4MjY3LCJzY29wZXMiOlsicmVnaXN0cmF0aW9uX2dldF93aWZpX3NldHRpbmdzIl19.yfWqqg_zg_TjH0tyIWkU_8agcSSemCDOYBA4bqApOhi8Ygji5lC5Yf3-tU2kt-zT"
+
+    private var wifi: WifiSession? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        wifi = WifiSession.Builder(this)
-            .apiKey("pntr0-dhjopjl43-jH5nfvOmmljbdf")
-            .userId("pntr54355430-dfbdb43-43t34mmljbdf")
-            .channelId(1)
-            .projectId(1)
-            .statusCallback(object : WifiSessionCallback {
-                override fun onStatusChanged(newStatus: WiFiSessionStatus) {
-                    println("Wifi status changed: $newStatus")
-                }
-            })
-            .create()
+        binding.inputToken.apply {
+            setHint("x-api-key")
+
+            var token = cache.apiToken
+            if (token.isEmpty()) token = API_KEY
+
+            setText(token)
+        }
+        binding.inputUserId.apply {
+            setHint("user id")
+
+            var id = cache.userId
+            if (id.isEmpty()) id = "pntr54355430-dfbdb43-43t34mmljbdf"
+            setText(id)
+        }
+        binding.inputProjectId.apply {
+            setHint("project id")
+            setText(cache.projectId)
+        }
+        binding.inputChannelId.apply {
+            setHint("channel id")
+            setText(cache.channelId)
+        }
+        binding.btContinue.setOnClickListener {
+            doConnect()
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        wifi.startSession()
+    private fun doConnect() {
+        try {
+            val channel = binding.inputChannelId.getText().toInt()
+            val project = binding.inputProjectId.getText().toInt()
+
+            wifi = WifiSession.Builder(this)
+                .apiKey(binding.inputToken.getText())
+                .userId(binding.inputUserId.getText())
+                .channelId(channel)
+                .projectId(project)
+                .statusCallback(object : WifiSessionCallback {
+                    override fun onStatusChanged(newStatus: WiFiSessionStatus) {
+                        binding.tvStatus.text =
+                            resources.getString(R.string.connect_status, newStatus)
+                    }
+                })
+                .create()
+            wifi?.startSession()
+        } catch (e: Throwable) {
+            binding.tvStatus.text = resources.getString(
+                R.string.connect_status, WiFiSessionStatus.Error(
+                    Exception(e)
+                )
+            )
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        cache.apiToken = binding.inputToken.getText()
+        cache.userId = binding.inputUserId.getText()
+        cache.channelId = binding.inputChannelId.getText()
+        cache.projectId = binding.inputProjectId.getText()
     }
 
     override fun onDestroy() {
-        wifi.cancelSession()
+        wifi?.cancelSession()
         super.onDestroy()
     }
 }
