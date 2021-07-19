d1d766312d5d76972986f33ff50d7b94eaff1782
obolsh <obolsh@gmail.com>
2021-07-19 12:50:13 +0300

    Update connection name

--- sdk/src/main/java/io/connect/wifi/sdk/Constants.kt
+++ sdk/src/main/java/io/connect/wifi/sdk/Constants.kt
@@ -6,7 +6,7 @@ internal class Constants {
         const val TYPE_WPA2_SUPPORT = "wpa2_old_method"
         const val TYPE_WPA2_SUGGESTION = "wpa2_suggestion_method"
         const val TYPE_PASSPOINT_AOUCP = "passpoint_aoupc_method"
-        const val TYPE_WPA2_ENTERPRISE_SUGGESTION = "wpa2_enterprise_suggestion_method"
+        const val TYPE_WPA2_ENTERPRISE_SUGGESTION = "wpa2_enterprise_eap_ttls_suggestion_method"
         const val TYPE_WPA2_PROFILE = "wpa2_profile"
     }
 }
